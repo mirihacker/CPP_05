6196a8e457d26f97988c2ebfbaece993e40334ee
Siria <siria_miren@icloud.com>
2025-09-11 18:14:46 +0200

    final

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: smiranda <smiranda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/11 14:41:33 by smiranda          #+#    #+#             */
-/*   Updated: 2025/09/11 17:46:25 by smiranda         ###   ########.fr       */
+/*   Updated: 2025/09/11 18:12:35 by smiranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,18 @@
 
 int main()
 {
-    std::cout << "\n--*--*-- BUREAUCRAT CREATION TEST --*--*--\n" << std::endl;
+    std::cout << "\n\033[33m--*--*-- BUREAUCRAT CREATION TEST --*--*--\n" << "\033[0m" << std::endl;
 
     try
     {
         std::cout << "Creating Bureaucrat Juan grade 5..." << std::endl;
         Bureaucrat Juan("Juan", 5);
-        std::cout << Juan;
-        while (Juan.getGrade() > 1)
+        while (Juan.getGrade() >= 1)
         {
+            std::cout << "\033[32m" << Juan << "\033[0m";
             Juan.incrementGrade();
-            std::cout << Juan;
         }
-        std::cout << "\nIncrementing over the limit!!!...\n" << std::endl;
+        std::cout << "\n\033[31mIncrementing over the limit!!!...\n" << "\033[0m" << std::endl;
         Juan.incrementGrade();
     }
     catch(const std::exception& e)
@@ -35,11 +34,11 @@ int main()
         std::cerr << e.what();
     }
 
-    std::cout << "\n--*--*-- INVALID BUREAUCRAT TEST --*--*--\n" << std::endl;
+    std::cout << "\n\033[33m--*--*-- INVALID BUREAUCRAT TEST --*--*--\n" << "\033[0m" << std::endl;
 
     try
     {
-        std::cout << "Creating Bureaucrat Jose grade 151 (should throw exception)" << std::endl;
+        std::cout << "\033[31mCreating Bureaucrat Jose grade 151 (should throw exception)" << "\033[0m" << std::endl;
         Bureaucrat Jose("Jose", 151);
     }
     catch(const std::exception& e)
@@ -49,7 +48,7 @@ int main()
     }
     try
     {
-        std::cout << "Creating Bureaucrat Juanjo grade 0 (should throw exception)" << std::endl;
+        std::cout << "\033[31mCreating Bureaucrat Juanjo grade 0 (should throw exception)" << "\033[0m" << std::endl;
         Bureaucrat Juanjo("Juanjo", 0);
     }
     catch(const std::exception& e)
